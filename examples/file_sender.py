6244e7a3a818bfccc3f364b264bf8d454ed9a64b
KhagendraN <nkhagendra1@gmail.com>
2025-07-16 19:35:00 +0545

    examples updated with proper logging

--- examples/file_sender.py
+++ examples/file_sender.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
     send_file("localhost", 8080, "Assets/test2.json")
     send_file("localhost", 8080, "Assets/Sahanavhabatu.mp3")
     # after execution files should be available on Assets/server/ directory  
+
+# --- Example: File send with progress bar ---
+# from kn_sock import send_file
+# send_file('localhost', 8080, 'file.txt', show_progress=True) 

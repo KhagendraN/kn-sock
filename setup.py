b4eb5935e374c4b5e25c4062b2373ac955bb179b
KhagendraN <nkhagendra1@gmail.com>
2025-07-22 07:14:02 +0545

    build: update setup.py to support new Python versions and dependency constraints

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup, find_packages
+
+# Get absolute path to the directory containing this file
+this_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Read the long description from docs/index.md
+with open(os.path.join(this_dir, "docs", "index.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="kn-sock",
@@ -13,7 +21,7 @@ setup(
     author="Khagendra Neupane",
     author_email="nkhagendra1@gmail.com",
     description="A simplified socket programming toolkit for Python",
-    long_description=open("docs/index.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KhagendraN/kn-sock",
     classifiers=[
